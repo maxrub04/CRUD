150efc8aeefefff925ac4387e004c3e815097c12
Maksym Rubtsov <maksrub04@gmail.com>
2025-06-03 17:07:36 +0200

    reset

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import re
 import json
+import User_Model
+import User_Manager
 
-class User:
+while True:
+        print("\n--- User CRUD Menu ---")
+        print("1. Create User")
+        print("2. View User")
+        print("3. Update User")
+        print("4. Delete User")
+        print("5. List All Users")
+        print("6. Exit")
 
-    def __init__(self, username, name, email, phone, age):
-        self.username = username
-        self.name = name
-        self.email = email
-        self.phone = phone
-        self.age = age
+        choice = input("Enter choice (1–6): ")
 
-
-
-class Validator:
-    def __init__(self, username, name, email, phone, age):
-        self.username = username
-        self.name = name
-        self.email = email
+        if choice == '1':
+            User.create_user()
+        elif choice == '2':
+            User.view_user()
+        elif choice == '3':
+            User.update_user()
+        elif choice == '4':
+            User.delete_user()
+        elif choice == '5':
+            User.list_users()
+        elif choice == '6':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Try again.")
