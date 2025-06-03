857ef4580ba7b036223f65952ab82bb52961da3e
Maksym Rubtsov <maksrub04@gmail.com>
2025-06-03 17:04:50 +0200

    static methods

--- User_Manager.py
+++ User_Manager.py
@@ -18,3 +18,41 @@ class User:
             "phone": self.phone,
             "age": self.age
         }
+
+    @staticmethod
+    def validate_username(username, existing_users):
+        if not username.strip():
+            print("Username cannot be empty")
+            return False
+        if username in existing_users:
+            print("Username already exists")
+            return False
+        return True
+
+    @staticmethod
+    def validate_name(name):
+        if not name.strip():
+            print("Name cannot be empty")
+
+    @staticmethod
+    def validate_email(email):
+        if not email.strip():
+            print("Email cannot be empty")
+            return False
+        if not re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email):
+            print("Invalid email address")
+            return False
+
+    @staticmethod
+    def validate_phone(phone):
+        if not phone.strip():
+            print("Phone cannot be empty.")
+
+    @staticmethod
+    def validate_age(age):
+        if not age.strip():
+            print("Age cannot be empty.")
+        if not age.isdigit():
+            print("Age must be a number.")
+        if int(age) <= 0:
+            print("Age must be greater than 0.")
